682c38aa60c6eccff4cfd90eb43a02414ea92ac2
Tommy Lay <tommylay.c@gmail.com>
2026-03-06 14:09:00 -0800

    ui: added title of note to lookup display

--- src/modules/lookup/ui/components/note-list.tsx
+++ src/modules/lookup/ui/components/note-list.tsx
@@ -5,7 +5,11 @@ const NoteList = ({ notes }: { notes: NoteResponse[] }) => {
   console.log(notes);
   return notes.map((note) => (
     <div key={note.id} className="p-3">
-      <h1 className="font-bold">{note.title}</h1>
+      <div className="flex justify-between">
+        <h1 className="font-bold">{note.title}</h1>
+        {/* TODO: format the ugly looking date  */}
+        <h3 className="font-semibold">{note.creation_date}</h3>
+      </div>
       <p>{note.content}</p>
       <TagList tags={note.tags} />
     </div>
