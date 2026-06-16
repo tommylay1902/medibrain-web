05fc3aa04da475c2e5b9cb6f547b1e27023204b7
Tommy Lay <tommylay.c@gmail.com>
2026-06-15 17:47:57 -0700

    feat: added thinking animation when waiting for llm to respond

--- src/modules/lookup/ui/components/document-view.tsx
+++ src/modules/lookup/ui/components/document-view.tsx
@@ -3,7 +3,7 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ArrowUpRightIcon, Loader2 } from "lucide-react"; // or any spinner component
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Response = {
   content: string;
@@ -15,11 +15,18 @@ type Response = {
 const DocumentView = () => {
   const params = useSearchParams();
   const [results, setResults] = useState<string[]>([]);
+  const [isStreaming, setIsStreaming] = useState<boolean>(false);
 
   const search = params.get("search")?.trim();
 
   useEffect(() => {
-    if (!search || search == "") return;
+    if (!search || search === "" || search.length === 0) return;
+
+    console.log("enter");
+
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setResults([]);
+    setIsStreaming(true);
 
     const source = new EventSource(
       `${process.env.NEXT_PUBLIC_MEDIBRAIN_API_BASE_URL}/document/query?search=${search}`,
@@ -28,15 +35,20 @@ const DocumentView = () => {
 
     source.onmessage = (e) => {
       if (e.data === "[DONE]") {
+        setIsStreaming(false);
         source.close();
         return;
       } else {
         setResults((prev) => [...prev, e.data]);
       }
     };
-    source.onerror = () => source.close();
+    source.onerror = () => {
+      setIsStreaming(false);
+      source.close();
+    };
 
     return () => {
+      setIsStreaming(false);
       setResults([]);
       source.close();
     };
@@ -46,7 +58,9 @@ const DocumentView = () => {
     <div className="">
       <h3 className="mx-5 font-bold">Documents</h3>
       <ScrollArea className="mx-5 rounded-md h-84 border-black border">
-        {!results || results.length === 0 ? (
+        {isStreaming && results.length === 0 ? (
+          <p className=" text-muted-foreground animate-pulse">Thinking...</p>
+        ) : results.length === 0 ? (
           <h2 className="flex justify-center items-center font-bold">
             No documents found please try another search
           </h2>
